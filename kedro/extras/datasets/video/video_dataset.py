eef20d4fc85d96c8274bf23e2dcd23bfe3b1ed65
Daniel Falk <Daniel.Falk.1@FixedIT.ai>
2022-02-26 12:46:09 +0100

    Add a simple implementation of save function for video
    See the dataset file for limitations.
    
    Signed-off-by: Daniel Falk <Daniel.Falk.1@FixedIT.ai>

--- kedro/extras/datasets/video/video_dataset.py
+++ kedro/extras/datasets/video/video_dataset.py
@@ -13,9 +13,6 @@ import PIL.Image
 import numpy as np
 
 
-# TODO: Implement save
-
-
 class SlicedVideo:
     def __init__(self, video, slice_indexes):
         self.video = video
@@ -37,7 +34,22 @@ class Video:
     def __init__(self, filepath: str) -> None:
         self._cap = cv2.VideoCapture(filepath)
         self._n_frames = self._cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        self.index = 0 # Next available frame
+        self.index = 0  # Next available frame
+
+    @property
+    def fourcc(self):
+        fourcc = self._cap.get(cv2.CAP_PROP_FOURCC)
+        return int(fourcc).to_bytes(4, "little").decode("ascii")
+
+    @property
+    def fps(self):
+        return self._cap.get(cv2.CAP_PROP_FPS)
+
+    @property
+    def size(self):
+        width = int(self._cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        return width, height
 
     def __getitem__(self, index: int):
         if isinstance(index, slice):
@@ -50,8 +62,12 @@ class Video:
             self._cap.set(cv2.CAP_PROP_POS_FRAMES, index)
         self.index = index + 1  # Next frame to decode after this
         ret, frame_bgr = self._cap.read()
+        if not ret:
+            raise IndexError()
         height, width = frame_bgr.shape[:2]
-        return PIL.Image.frombuffer("RGB", (width, height), frame_bgr, "raw", "BGR", 0, 0)
+        return PIL.Image.frombuffer(
+            "RGB", (width, height), frame_bgr, "raw", "BGR", 0, 0
+        )
 
     def __len__(self) -> int:
         # OpenCV's frame count might be an approximation depending on what
@@ -94,11 +110,29 @@ class VideoDataSet(AbstractDataSet):
         with self._fs.open(load_path, mode="r") as f:
             return Video(load_path)
 
-    def _save(self, data: PIL.Image) -> None:
-        """Saves image data to the specified filepath."""
+    def _save(self, video: Video) -> None:
+        """Saves image data to the specified filepath.
+
+        TODO: This can only save video from video file, i.e. one cant modify the video.
+        Create subclasses of Video so that videos can be created from an iterable of frames or
+        from a generator yielding frames.
+        """
         save_path = get_filepath_str(self._filepath, self._protocol)
-        with self._fs.open(save_path, mode="wb") as f:
-            raise NotImplementedError("Saving video is not yet supported")
+        # TODO: This assumes that the output file has the same fourcc code as the input file,
+        # this might not be the case since we can use one VideoDataSet to read e.g. a mp4 file with H264 video
+        # and then save it to another VideoDataSet which should use an .avi file with MJPEG
+        # TODO: There is no way to use the OpenVN VideoWrite to write to an open file, so it does not
+        # work together with fsspec. Investigate this further...
+        writer = cv2.VideoWriter(
+            save_path, cv2.VideoWriter_fourcc(*video.fourcc), video.fps, video.size
+        )
+        try:
+            for frame in video:
+                writer.write(  # PIL images are RGB, opencv expects BGR
+                    np.array(frame)[:, :, ::-1]
+                )
+        finally:
+            writer.release()
 
     def _describe(self) -> Dict[str, Any]:
         return dict(filepath=self._filepath, protocol=self._protocol)
