fb3264f3509496026aa6843b3ef47b332ddcf6c8
Daniel Falk <daniel.falk.1@fixedit.ai>
2022-05-31 11:09:53 +0200

    Add unit tests and fix lining and type checks
    Signed-off-by: Daniel Falk <daniel.falk.1@fixedit.ai>

--- kedro/extras/datasets/video/video_dataset.py
+++ kedro/extras/datasets/video/video_dataset.py
@@ -1,20 +1,24 @@
-from pathlib import PurePosixPath
-from typing import Any, Dict, Iterable, Generator, Union
-
-from kedro.io.core import (
-    AbstractDataSet,
-    get_protocol_and_path,
-)
-
-import fsspec
-import cv2
-import PIL.Image
-import numpy as np
-import more_itertools
+"""``VideoDataSet`` loads/saves video data from an underlying
+filesystem (e.g.: local, S3, GCS). It uses OpenCV VideoCapture to read
+and decode videos and OpenCV VideoWriter to encode and write video.
+"""
 import tempfile
+from collections import abc
+from pathlib import PurePosixPath
+from typing import Any, Dict, Generator, Optional, Sequence, Tuple, Union
+
+import cv2
+import fsspec
+import more_itertools
+import numpy as np
+import PIL.Image
+
+from kedro.io.core import AbstractDataSet, get_protocol_and_path
 
 
 class SlicedVideo:
+    """A representation of slices of other video types"""
+
     def __init__(self, video, slice_indexes):
         self.video = video
         self.indexes = range(*slice_indexes.indices(len(video)))
@@ -31,47 +35,54 @@ class SlicedVideo:
         return getattr(self.video, item)
 
 
-class AbstractVideo:
-    def __init__(self):
-        raise NotImplementedError("AbstractVideo should be extended")
+class AbstractVideo(abc.Sequence):
+    """Base class for the underlying video data"""
+
+    _n_frames = 0
+    _index = 0  # Next available frame
 
     @property
-    def fourcc(self):
+    def fourcc(self) -> str:
+        """Get the codec fourcc specification"""
         raise NotImplementedError()
 
     @property
-    def fps(self):
+    def fps(self) -> float:
+        """Get the video frame rate"""
         raise NotImplementedError()
 
     @property
-    def size(self):
+    def size(self) -> Tuple[int, int]:
+        """Get the resolution of the video"""
         raise NotImplementedError()
 
     def __len__(self) -> int:
-        return int(round(self._n_frames))
+        return self._n_frames
+
+    def __getitem__(self, index: Union[int, slice]):
+        """Get a frame from the video"""
+        raise NotImplementedError()
 
 
 class FileVideo(AbstractVideo):
     """A video object read from a file"""
 
     def __init__(self, filepath: str) -> None:
+        self._filepath = filepath
         self._cap = cv2.VideoCapture(filepath)
-        # OpenCV's frame count might be an approximation depending on what
-        # headers are available in the video file
-        self._n_frames = self._cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        self.index = 0  # Next available frame
+        self._n_frames = self._get_length()
 
     @property
-    def fourcc(self):
+    def fourcc(self) -> str:
         fourcc = self._cap.get(cv2.CAP_PROP_FOURCC)
         return int(fourcc).to_bytes(4, "little").decode("ascii")
 
     @property
-    def fps(self):
+    def fps(self) -> float:
         return self._cap.get(cv2.CAP_PROP_FPS)
 
     @property
-    def size(self):
+    def size(self) -> Tuple[int, int]:
         width = int(self._cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         return width, height
@@ -82,42 +93,56 @@ class FileVideo(AbstractVideo):
 
         if index < 0:
             index += len(self)
+        if index >= len(self):
+            raise IndexError()
 
-        if index != self.index:
+        if index != self._index:
             self._cap.set(cv2.CAP_PROP_POS_FRAMES, index)
-        self.index = index + 1  # Next frame to decode after this
+        self._index = index + 1  # Next frame to decode after this
         ret, frame_bgr = self._cap.read()
         if not ret:
             raise IndexError()
+
         height, width = frame_bgr.shape[:2]
         return PIL.Image.frombuffer(  # Convert to PIL image with RGB instead of BGR
             "RGB", (width, height), frame_bgr, "raw", "BGR", 0, 0
         )
 
+    def _get_length(self) -> int:
+        # OpenCV's frame count might be an approximation depending on what
+        # headers are available in the video file
+        length = int(round(self._cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+        if length >= 0:
+            return length
 
-class IterableVideo(AbstractVideo):
-    """A video object read from an iterable of frames"""
+        # On some video files it fails, we can count the frames but it will be
+        # very slow... It also seems like that opencv has issues to seek in those
+        # videos...
+        raise ValueError("Failed to load video since number of frames can't be infered")
+
+
+class SequenceVideo(AbstractVideo):
+    """A video object read from an indexable sequence of frames"""
 
     def __init__(
-        self, frames: Iterable[PIL.Image.Image], fps: float, fourcc: str = "avc1"
+        self, frames: Sequence[PIL.Image.Image], fps: float, fourcc: str = "avc1"
     ) -> None:
         self._n_frames = len(frames)
         self._frames = frames
         self._fourcc = fourcc
         self._size = frames[0].size
         self._fps = fps
-        self.index = 0  # Next available frame
 
     @property
-    def fourcc(self):
+    def fourcc(self) -> str:
         return self._fourcc
 
     @property
-    def fps(self):
+    def fps(self) -> float:
         return self._fps
 
     @property
-    def size(self):
+    def size(self) -> Tuple[int, int]:
         return self._size
 
     def __getitem__(self, index: Union[int, slice]):
@@ -143,16 +168,19 @@ class GeneratorVideo(AbstractVideo):
         self._fps = fps
 
     @property
-    def fourcc(self):
+    def fourcc(self) -> str:
         return self._fourcc
 
     @property
-    def fps(self):
+    def fps(self) -> float:
         return self._fps
 
     @property
-    def size(self):
+    def size(self) -> Tuple[int, int]:
         return self._size
+
+    def __getitem__(self, index: Union[int, slice]):
+        raise NotImplementedError("Underlying video is a generator")
 
     def __next__(self):
         return next(self._gen)
@@ -162,8 +190,8 @@ class GeneratorVideo(AbstractVideo):
 
 
 class VideoDataSet(AbstractDataSet):
-
-    """``VideoDataSet`` loads / save video data from a given filepath as sequence of PIL.Image.Image using OpenCV.
+    """``VideoDataSet`` loads / save video data from a given filepath as sequence
+    of PIL.Image.Image using OpenCV.
 
     Example:
     ::
@@ -174,16 +202,20 @@ class VideoDataSet(AbstractDataSet):
         >>> np.sum(np.asarray(frame))
     """
 
-    def __init__(self, filepath: str):
+    def __init__(self, filepath: str, fourcc: Optional[str] = "mp4v"):
         """Creates a new instance of VideoDataSet to load / save video data for given filepath.
 
         Args:
             filepath: The location of the video file to load / save data.
+            fourcc: The codec to use when writing video, note that depending on how opencv is
+                    installed there might be more or less codecs avaiable. If set to None, the
+                    fourcc from the video object will be used.
         """
         # parse the path and protocol (e.g. file, http, s3, etc.)
         protocol, path = get_protocol_and_path(filepath)
         self._protocol = protocol
         self._filepath = PurePosixPath(path)
+        self._fourcc = fourcc
 
     def _load(self) -> AbstractVideo:
         """Loads data from the video file.
@@ -192,40 +224,48 @@ class VideoDataSet(AbstractDataSet):
             Data from the video file as a AbstractVideo object
         """
         with fsspec.open(
-            "filecache::%s://%s" % (self._protocol, self._filepath), mode="rb"
-        ) as f:
-            return FileVideo(f.name)
+            f"filecache::{self._protocol}://{self._filepath}", mode="rb"
+        ) as fs_file:
+            return FileVideo(fs_file.name)
 
-    def _save(self, video: AbstractVideo) -> None:
+    def _save(self, data: AbstractVideo) -> None:
         """Saves video data to the specified filepath."""
         if self._protocol == "file":
             # Write directly to the local file destination
-            self._write_to_filepath(video, str(self._filepath))
+            self._write_to_filepath(data, str(self._filepath))
         else:
             # VideoWriter cant write to an open file object, instead write to a
             # local tmpfile and then copy that to the destination with fsspec
             with tempfile.NamedTemporaryFile(
-                suffix=self._filepath.suffix, mode="wb"
-            ) as tmp:
-                self._write_to_filepath(video, tmp.name)
+                suffix=self._filepath.suffix, mode="w+b"
+            ) as tmp_file:
+                self._write_to_filepath(data, tmp_file.name)
                 with fsspec.open(
-                    "%s://%s" % (self._protocol, self._filepath), "wb"
+                    f"{self._protocol}://{self._filepath}", "wb"
                 ) as f_target:
-                    with open(tmp.name, "rb") as f_tmp:
-                        f_target.write(f_tmp.read())
+                    f_target.write(tmp_file.read())
 
     def _write_to_filepath(self, video: AbstractVideo, filepath: str) -> None:
-        # TODO: This takes the the fourcc code (codec) from the Video object, the video object does not know what
-        # container format will be used since that is selected by the suffix in the file name. Some combinations
-        # of codec and container format might not work or will have bad support. There should at least be a way to
-        # set a video to no prefered codec and then select one based on the file suffix. If the video object is a
-        # FileVideo, then the fourcc code is set from the read file which might have another suffix/container format
-        # then the file we are writing to.
+        # TODO: This uses the codec specified in the VideoDataSet if it is not None, this is due
+        # to compatibility issues since e.g. h264 coded is licensed and is thus not included in
+        # opencv if installed from a binary distribution. Since a h264 video can be read, but not
+        # written, it would be error prone to use the videos fourcc code. Further, an issue is
+        # that the video object does not know what container format will be used since that is
+        # selected by the suffix in the file name of the VideoDataSet. Some combinations of codec
+        # and container format might not work or will have bad support.
+        fourcc = self._fourcc or video.fourcc
+
         writer = cv2.VideoWriter(
-            filepath, cv2.VideoWriter_fourcc(*video.fourcc), video.fps, video.size
+            filepath, cv2.VideoWriter_fourcc(*fourcc), video.fps, video.size
         )
+        if not writer.isOpened():
+            raise ValueError(
+                "Failed to open video writer with params: "
+                + f"fourcc={fourcc} fps={video.fps} size={video.size[0]}x{video.size[1]} "
+                + f"path={filepath}"
+            )
         try:
-            for frame in video:
+            for frame in iter(video):
                 writer.write(  # PIL images are RGB, opencv expects BGR
                     np.asarray(frame)[:, :, ::-1]
                 )
@@ -234,3 +274,7 @@ class VideoDataSet(AbstractDataSet):
 
     def _describe(self) -> Dict[str, Any]:
         return dict(filepath=self._filepath, protocol=self._protocol)
+
+    def _exists(self) -> bool:
+        fsystem = fsspec.filesystem(self._protocol)
+        return fsystem.exists(self._filepath)
---
+++ tests/extras/datasets/video/test_video_dataset.py
@@ -0,0 +1,131 @@
+import itertools
+from pathlib import Path
+
+import boto3
+import numpy as np
+import pytest
+from moto import mock_s3
+from PIL import ImageChops
+
+from kedro.extras.datasets.video import VideoDataSet
+from kedro.extras.datasets.video.video_dataset import FileVideo
+from kedro.io import DataSetError
+
+S3_BUCKET_NAME = "test_bucket"
+S3_KEY_PATH = "video"
+S3_FULL_PATH = f"s3://{S3_BUCKET_NAME}/{S3_KEY_PATH}/"
+
+
+@pytest.fixture
+def filepath_mp4():
+    return str(Path(__file__).parent / "data/video.mp4")
+
+
+@pytest.fixture
+def filepath_mjpeg():
+    return str(Path(__file__).parent / "data/video.mjpeg")
+
+
+@pytest.fixture
+def tmp_filepath_mp4(tmp_path):
+    return str((tmp_path / "test.mp4").as_posix())
+
+
+@pytest.fixture
+def tmp_filepath_avi(tmp_path):
+    return str((tmp_path / "test.mjpeg").as_posix())
+
+
+@pytest.fixture
+def empty_dataset_mp4(tmp_filepath_mp4):
+    return VideoDataSet(filepath=tmp_filepath_mp4)
+
+
+@pytest.fixture
+def empty_dataset_avi(tmp_filepath_avi):
+    return VideoDataSet(filepath=tmp_filepath_avi)
+
+
+@pytest.fixture
+def mp4_object(filepath_mp4):
+    return FileVideo(filepath_mp4)
+
+
+@pytest.fixture
+def mjpeg_object(filepath_mjpeg):
+    return FileVideo(filepath_mjpeg)
+
+
+@pytest.fixture
+def mocked_s3_bucket():
+    """Create a bucket for testing using moto."""
+    with mock_s3():
+        conn = boto3.client("s3")
+        conn.create_bucket(Bucket=S3_BUCKET_NAME)
+        yield conn
+
+
+def assert_images_equal(image_1, image_2):
+    """Assert that two images are approximately equal, allow for some
+    compression artifacts"""
+    assert image_1.size == image_2.size
+    diff = np.asarray(ImageChops.difference(image_1, image_2))
+    assert np.mean(diff) < 5
+    assert np.mean(diff > 50) < 0.01  # Max 1% of pixels
+
+
+def assert_videos_equal(video_1, video_2):
+    assert len(video_1) == len(video_2)
+
+    for image_1, image_2 in itertools.zip_longest(video_1, video_2):
+        assert_images_equal(image_1, image_2)
+
+
+class TestVideoDataSet:
+    def test_load_mp4(self, filepath_mp4, mp4_object):
+        """Loading a mp4 dataset should create a FileVideo"""
+        ds = VideoDataSet(filepath_mp4)
+        loaded_video = ds.load()
+        assert_videos_equal(loaded_video, mp4_object)
+
+    def test_save_and_load_mp4(self, empty_dataset_mp4, mp4_object):
+        """Test saving and reloading the data set."""
+        empty_dataset_mp4.save(mp4_object)
+        reloaded_video = empty_dataset_mp4.load()
+        assert_videos_equal(mp4_object, reloaded_video)
+
+    def test_exists(self, empty_dataset_mp4, mp4_object):
+        """Test `exists` method invocation for both existing and
+        nonexistent data set."""
+        assert not empty_dataset_mp4.exists()
+        empty_dataset_mp4.save(mp4_object)
+        assert empty_dataset_mp4.exists()
+
+    @pytest.mark.skip(reason="Can't deal with videos with missing time info")
+    def test_convert_video(self, empty_dataset_mp4, mjpeg_object):
+        """Load a file video in mjpeg format and save in mp4v"""
+        empty_dataset_mp4.save(mjpeg_object)
+        reloaded_video = empty_dataset_mp4.load()
+        assert_videos_equal(mjpeg_object, reloaded_video)
+
+    def test_load_missing_file(self, empty_dataset_mp4):
+        """Check the error when trying to load missing file."""
+        pattern = r"Failed while loading data from data set VideoDataSet\(.*\)"
+        with pytest.raises(DataSetError, match=pattern):
+            empty_dataset_mp4.load()
+
+    def test_save_s3(self, mp4_object, mocked_s3_bucket, tmp_path):
+        """Test to save a VideoDataSet to S3 storage"""
+        video_name = "video.mp4"
+
+        dataset = VideoDataSet(filepath=S3_FULL_PATH + video_name)
+        dataset.save(mp4_object)
+
+        tmp_file = tmp_path / video_name
+        mocked_s3_bucket.download_file(
+            Bucket=S3_BUCKET_NAME,
+            Key=S3_KEY_PATH + "/" + video_name,
+            Filename=str(tmp_file),
+        )
+        reloaded_video = FileVideo(str(tmp_file))
+        assert_videos_equal(reloaded_video, mp4_object)
